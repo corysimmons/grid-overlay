0d8729367d99e438e71f69c45c500e0d4b2c8098
Cory Simmons <csimmonswork@gmail.com>
2014-08-31 12:39:21 -0400

    Add go-parent

--- gridOverlay.js
+++ gridOverlay.js
@@ -2,14 +2,17 @@ var gridOverlay = function() {
   $('.grid-overlay').remove();
   $('.go').each(function(k, v) {
     $('body').prepend('<div class="grid-overlay" style="width: '+ $(this).width() +'px; left: '+ $(this).position().left +'px" />');
-    $('.grid-overlay').css({
-      background: 'blue',
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      opacity: .15,
-      zIndex: 9999
-    });
+  });
+  $('[go-parent]').children().each(function(k, v) {
+    $('body').prepend('<div class="grid-overlay" style="width: '+ $(this).width() +'px; left: '+ $(this).position().left +'px" />');
+  });
+  $('.grid-overlay').css({
+    background: 'blue',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    opacity: .15,
+    zIndex: 9999
   });
 };
 
